b11271e9d0e8a97e96de3e3b7c7992073f74e2c2
Toria <tzimms7@gmail.com>
2019-10-28 11:44:51 +0000

    fetch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import DisplayQuote from './DisplayQuote';
 
 import Quote from "./Quote";
 // import Quotes from "./Quotes";
@@ -9,17 +10,34 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      working: true
-    }
+      working: true,
+      APIquote: {},
+      isLoading: true,
+    };
   }
   handleClick = () => {
     this.setState({ working: !this.state.working })
   }; 
+
+  getQuote = () => {
+    fetch("https://randomuser.me/api?nat=fr")
+      .then(response  =>  response.json())
+      .then(data  => {
+      // console.log(data)
+          this.setState( {
+              APIquote: data.results[0],
+              isLoading: false
+            });
+        });
+    }
+
+
   render() {
     let homer = this.state.working? "App-logo": "App-logo-stop"; 
     return (
+      <>
       <div className="App">
-        <header className="App-header">
+         <header className="App-header">
           <img src={logo} className={homer} alt="logo" /> 
           <button
           onClick={this.handleClick}
@@ -27,19 +45,27 @@ class App extends Component {
             {'Click Me!'}
         </button>
           <h1 className="App-title">Simpsons Quotes</h1>
-        </header>
-       
-        <Quote
+        </header> 
+        <button onClick={this.getQuote} >
+          'Change Quote'
+        </button>
+        { !this.state.isLoading && 
+          <DisplayQuote
+            APIquote={this.state.APIquote} 
+          />
+        }
+      </div>
+       <Quote
           quote="I believe the children are the future... Unless we stop them now!"
           character="Homer Simpson"
           image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
-        />
+       />
          <Quote
           quote="Me fail English? That's unpossible"
           character="Ralph Wiggum"
           image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523"
-        />
-      </div>
+        /> 
+      </>
     );
   }
 }
